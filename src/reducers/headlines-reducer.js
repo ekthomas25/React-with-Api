c15ecd5fba8b1b383b4c8947049085feefaf4721
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-02-27 17:44:23 -0800

    Pass test 2

--- src/reducers/headlines-reducer.js
+++ src/reducers/headlines-reducer.js
@@ -1,9 +1,18 @@
-const defaultState = {
+import * as c from './../actions/ActionTypes';
+
+let initialState = {
   isLoading: false,
   headlines: [],
   error: null
 }
 
 export default (state = defaultState, action) => {
-  return state;
+  switch(action.type) {
+    case c.REQUEST_HEADLINES:
+      return Object.assign({}, state, {
+        isLoading: true
+      });
+    default:
+      return state;
+  }
 };
